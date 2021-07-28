100e801c8170b61268f298ee7e4cc2813b2c63e1
utkarsh saini <utkarshsaini19@gmail.com>
2021-07-28 15:37:28 +0530

    simple project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Temp from './components/temp'
+
+
+function App() {
+ 
+  return (
+    <>
+      
+      <Temp />
+     
+
+    </>
+  );
+}
+
+export default App;
