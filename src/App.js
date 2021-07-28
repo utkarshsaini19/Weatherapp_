077fd6404eb7d809fb021634a9fd05f9f9948416
utkarsh saini <utkarshsaini19@gmail.com>
2021-07-28 16:00:32 +0530

    updated1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Temp from './components/temp'
-const { REACT_APP_API_ID } = process.env;
+
 
 function App() {
- 
+  const { REACT_APP_API_ID } = process.env;
   return (
     <>
       
